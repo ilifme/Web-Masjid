be117374b3e66830b18788589570b327b77b738f
ilifme <aliffahmi1500@gmail.com>
2026-06-11 08:50:37 +0700

    Fix Bearer token in api.js - was missing quotes and token variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,7 +15,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = Bearer ;
+      config.headers.Authorization = 'Bearer ' + token;
     }
     return config;
   },
